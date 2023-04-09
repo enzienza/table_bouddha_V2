78ab536df9c951f55ab9dbb2e9c5fcab78c1239f
EnzaEnzi <enza.webdev@gmail.com>
2023-04-09 19:23:35 +0200

    [front] emportes

--- page-emporters.php
+++ page-emporters.php
@@ -21,7 +21,48 @@
 <?php get_template_part('template-parts/page/emportes/intro'); ?>
 
 <!-- Section content -->
-Il n'y a pas de Boissons actuellement
+<section class="container main-takeawaypage my-5">
+    <ul class="nav nav-tabs">
+        <?php
+        wp_reset_postdata();
+
+        $args = array(
+            'post_type'      => 'emporters',
+            'posts_per_page' => -1,
+            'orderby'        => 'id',
+            'order'          => 'ASC'
+        );
+        $my_query = new WP_query($args);
+        if($my_query->have_posts()) : while($my_query->have_posts()) : $my_query->the_post();
+            ?>
+            <?php get_template_part('template-parts/post/nav-tabs'); ?>
+        <?php endwhile; endif;  wp_reset_postdata(); ?>
+    </ul><!--/.nav.nav-tabs-->
+
+    <div class="tab-content">
+        <div class="tab-pane fade show active">
+            <p class="else-display">
+                <?php echo get_option('msg_loop_takeawaypage') ?>
+            </p>
+        </div>
+        <?php
+        wp_reset_postdata();
+
+        $args = array(
+            'post_type'      => 'emporters',
+            'posts_per_page' => -1,
+            'orderby'        => 'id',
+            'order'          => 'ASC'
+        );
+        $my_query = new WP_query($args);
+        if($my_query->have_posts()) : while($my_query->have_posts()) : $my_query->the_post();
+            ?>
+            <?php get_template_part('template-parts/post/tab-content'); ?>
+        <?php endwhile; endif;  wp_reset_postdata(); ?>
+
+    </div><!--/.tab-content-->
+
+</section><!--/.main-takeawaypage-->
 
 <?php if(checked(1, get_option('show_on_takeawaypage'), false)) : ?>
     <!--  Section Reservation  -->
